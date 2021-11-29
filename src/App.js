8a9909f370a5032e9a38d78f341bc856cec8313e
macmima1111 <https://github.com/liuzhiq779/reactStudy.git>
2021-11-29 15:45:47 +0800

    react第二次提交（redux)dome功能

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          hellow world
-        </div>
-      
-      </header>
-    </div>
-  );
+import React,{Component} from "react";
+import 'antd/dist/antd.css'
+import { Input,Button,List } from 'antd'
+import store from './store/index';
+
+ 
+class readxStusy extends Component {
+    constructor(props){
+      super(props);
+      this.state = store.getState();
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleStoreChange = this.handleStoreChange.bind(this);
+      this.handleBtnClick = this.handleBtnClick.bind(this);
+      store.subscribe(this.handleStoreChange)
+    }
+
+
+    render() {
+        return (
+            <div style={{marginTop:'10px',marginLeft:'10px'}}>
+                <div>
+                    <Input 
+                    value={this.state.inputValue}
+                     placeholder='todo info' 
+                    style={{width: 300,marginRight: '10px'}}
+                    onChange={this.handleInputChange}
+                    />
+                    <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
+                    <List
+                        style={{marginTop:'10px',width:'800px'}}
+                        size="small"
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={(item,index) => <List.Item  onClick = {this.handleItemDelete.bind(this,index)}>{item}</List.Item>}
+                    />
+                </div>
+            </div>
+        ) 
+    }
+    handleInputChange(e){
+        console.log(e.target.value)
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleStoreChange(){
+       this.setState(store.getState()); 
+    }
+
+   handleBtnClick(){
+       const action = {
+           type:'add_todo_item'
+       }
+       store.dispatch(action);
+   }
+
+   handleItemDelete(index){
+     const action = {
+         type: 'delete_todo_item',
+         index
+     }
+     store.dispatch(action)
+  }
 }
 
-export default App;
+
+
+
+export default  readxStusy;
